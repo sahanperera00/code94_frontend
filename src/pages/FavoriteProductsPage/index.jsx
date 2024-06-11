16aaaacf1efe67ce4cf7a97d2c82eac413e9b551
Sahan Perera <sahanvimukthi00@gmail.com>
2024-06-11 18:10:58 +0530

    feat: favorite products page added

---
+++ src/pages/FavoriteProductsPage/index.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import Searchbar from "../../components/Searchbar";
+import Button from "../../components/Button";
+import Row from "../../components/TableRow";
+import StarredIcon from "../../assets/icons/starred.svg";
+import api, { setAuthToken } from "../../utils/api";
+import { useNavigate } from "react-router-dom";
+import Popup from "../../components/Popup";
+
+export default function FavoriteProducts() {
+  const [products, setProducts] = useState([]);
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    setAuthToken(token);
+
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get("/product/all");
+        setProducts(response.data.products);
+        console.log(products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="container bg-[#] mx-auto">
+      <h1 className="uppercase text-[36px] font-bold tracking-widest">
+        Favorite Products
+      </h1>
+      <div className="grid grid-cols-2 mb-5">
+        <Searchbar />
+        <div className="flex justify-end gap-3">
+          <Button
+            type="primary"
+            onClick={() => {
+              navigate("/add");
+            }}
+            className="px-16"
+          >
+            New Product
+          </Button>
+          <Button type="secondary" onClick={() => {}} className="px-5">
+            <img src={StarredIcon} alt="Edit" className="w-6 h-6" />
+          </Button>
+        </div>
+      </div>
+      <table className="container table-fixed text-lg font-medium">
+        <thead>
+          <tr className="text-[#001eb9] text-left uppercase">
+            <th>SKU</th>
+            <th>Image</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <Row key={product.id} product={product} setShow={setShow} />
+          ))}
+        </tbody>
+      </table>
+
+      <Popup show={show} setShow={setShow} />
+    </div>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import MainPage from "../pages/MainPage";
 import Login from "../pages/Login";
 import AddProduct from "../pages/AddProductPage";
 import EditProduct from "../pages/EditProductPage";
+import FavoriteProducts from "../pages/FavoriteProductsPage";
 
 export default function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
       <Route path="/main" element={<MainPage />} />
       <Route path="/add" element={<AddProduct />} />
       <Route path="/edit" element={<EditProduct />} />
+      <Route path="/favorites" element={<FavoriteProducts />} />
     </Routes>
   );
 }
